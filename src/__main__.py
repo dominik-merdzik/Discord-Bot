5245935f8ad9d74572c2aa87c03b80c9cae25cfe
dominik-merdzik <dominiktm@outlook.com>
2021-12-11 15:36:50 -0500

    added imports and changed command_prefix

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,15 @@
 # file app/__main__.py
 
 def main():
-    from discord.ext.commands import Bot
-    import discord.utils
     import random
     import secrets
+    import tkinter
+    import discord 
+
+    from discord.ext.commands import Bot
 
 
-    bot = Bot(command_prefix='!')
+    bot = Bot(command_prefix='.')
 
 
     #print bot info when it boots up
@@ -32,24 +34,11 @@ def main():
         elif rand == 1:
             await ctx.channel.send('Tails!')
 
-    # Command to make an announcement to whole server, but in a specific channel.
-    @bot.command(pass_context=True)
-    async def announce(ctx, subCommand: str, subSubCommand: str, arg: str):
-        # Used to configure settings related to this command.
-        if (subCommand == 'config'):
-            
-            # Used to configure which channel the bot should make announcements too.
-            if (subSubCommand == 'channel'):
-                # TODO:(Allow for the ability to modify channel for announcement)
-                channel = discord.utils.get(ctx.guild.channels, name=arg)
-                await channel.send('This is the new announcement channel.')
-
-
-
     # print message back
     @bot.command()
     async def repeat(ctx, *, arg):
         await ctx.send(arg)
+
 
     #Run bot
     bot.run(secrets.secretToken)
